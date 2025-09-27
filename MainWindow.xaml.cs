27f4b82993855ef2186c56b722a1a4ce6fb3eb57
Sean de beeg <seandepew2011@gmail.com>
2025-09-27 19:31:46 -0400

    Added custom Title bar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
 
 namespace WindowsSnake
 {
@@ -7,6 +9,10 @@ namespace WindowsSnake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isMaximized = false;
+        private double originalWidth;
+        private double originalHeight;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -14,7 +20,48 @@ namespace WindowsSnake
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-          MainNavigation.Navigate(new MainMenu(this));
+            MainNavigation.Navigate(new MainMenu(this));
+            originalWidth = this.Width;
+            originalHeight = this.Height;
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+          if (!isMaximized)
+          {
+            this.WindowState = WindowState.Maximized;
+            isMaximized = true;
+          }
+          else
+          {
+            this.WindowState = WindowState.Normal;
+            isMaximized = false;
+          }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            var fadeOut = new DoubleAnimation
+            {
+                From = 1.0,
+                To = 0.0,
+                Duration = TimeSpan.FromMilliseconds(100)
+            };
+            fadeOut.Completed += (s, a) => this.Close();
+            this.BeginAnimation(Window.OpacityProperty, fadeOut);
+        }
+
+        private void Drag_Window(object sender, MouseButtonEventArgs e)
+        {
+          if(e.ChangedButton == MouseButton.Left) 
+          {
+            this.DragMove();
+          }
         }
     }
 }
