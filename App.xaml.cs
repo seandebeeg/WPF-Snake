e340ead1d7c51a73fe9c8b1a2785e49914743887
Sean de beeg <seandepew2011@gmail.com>
2025-10-05 18:24:20 -0400

    Revert "Make DarkTheme"
    This reverts commit b9e939b0e2cde69ccae2583037f77bd0509461b6.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows;
-using System.IO;
-using System.Text.Json;
 
 namespace WindowsSnake
 {
@@ -9,44 +7,6 @@ namespace WindowsSnake
     /// </summary>
     public partial class App : Application
     {
-        public GameSettings LoadSettings()
-        {
-          var settingsPath = Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
-            "Snake",
-            "settings.json"
-          );
-
-          if (File.Exists(settingsPath))
-          {
-            var currentSettings = JsonSerializer.Deserialize<GameSettings>(File.ReadAllText(settingsPath));
-            return currentSettings;
-          }
-          else
-          {
-            return new GameSettings { CurrentColor = "#FF00FF00", Modifiers = [], HighScore = 0, Settings = [], Multiplier = 1, ScoreEntries = [] };
-          }
-        }
-
-        public void ApplyDarkTheme()
-        {
-          GameSettings settings = LoadSettings();
-          if (settings.Settings.Contains(new() { Name = "Dark Mode", Type = "Preference", IsEnabled = true }))
-          {
-            for (int i = Resources.MergedDictionaries.Count - 1; i >= 0; i--)
-            {
-              var dict = Resources.MergedDictionaries[i];
-              if (dict.Source != null && dict.Source.OriginalString.Contains("Theme"))
-                Resources.MergedDictionaries.RemoveAt(i);
-            }
-
-            var darkTheme = new ResourceDictionary
-            {
-              Source = new Uri("pack://application:,,,/DarkTheme.xaml")
-            };
-            Resources.MergedDictionaries.Add(darkTheme);
-          }
-        }
     }
 
 }
