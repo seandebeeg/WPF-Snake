76e706f2654a3895f22ee33100d764386d0d87dc
Sean de beeg <seandepew2011@gmail.com>
2025-10-10 21:17:53 -0400

    Fix formatting in GamePage.xaml.cs and make seperate ModifierLogic file to store all methods involving modifiers

--- GamePage.xaml.cs
+++ GamePage.xaml.cs
@@ -8,138 +8,53 @@ namespace WindowsSnake
     /// <summary>
     /// Interaction logic for GamePage.xaml
     /// </summary>
-    public partial class GamePage : Page
+  public partial class GamePage : Page
+  {
+    private MainWindow _parentWindow;
+    public GamePage(MainWindow parentWindow)
     {
-        private MainWindow _parentWindow;
-        public GamePage(MainWindow parentWindow)
-        {
-            _parentWindow = parentWindow;
-            _parentWindow.Title = "Game";
-            InitializeComponent();
-            RunPreGameFunctions();
-        }
+      _parentWindow = parentWindow;
+      _parentWindow.Title = "Game";
+      InitializeComponent();
+      RunPreGameFunctions();
+    }
 
-        private int[,] BoardArray;
-        private double PlayerSpeed;
+    private int[,] BoardArray;
+    private double PlayerSpeed;
 
-        private static GameSettings LoadSettings()
-        {
-          var settingsPath = Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
-            "Snake",
-            "settings.json"
-          );
+    private static GameSettings LoadSettings()
+    {
+      var settingsPath = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+        "Snake",
+        "settings.json"
+      );
 
-          if (File.Exists(settingsPath))
-          {
-            GameSettings currentSettings = JsonSerializer.Deserialize<GameSettings>(File.ReadAllText(settingsPath));
-            return currentSettings;
-          }
-          else
-          {
-            GameSettings defaultSettings = new GameSettings 
-            { 
-              CurrentColor = "#FF0000FF",
-              Modifiers = null,
-              Multiplier = 1,
-              Settings = null
-            };
-            File.WriteAllText(settingsPath, JsonSerializer.Serialize(defaultSettings));
-            return defaultSettings;
-          }
-        }
-
-      private void MakeBoard()
+      if (File.Exists(settingsPath))
       {
-        const int DefaultRows = 20;
-        const int DefaultColumns = 20;
-        double BoardMultiplier = 1.00;
-        int RowNumber = DefaultRows;
-        int ColumnNumber = DefaultColumns;
-        if (currentSettings.Modifiers != null)
-        {
-          foreach (ModifierItem Modifier in currentSettings.Modifiers)
-          {
-            if (Modifier.Name.Contains("Board"))
-            {
-              switch (Modifier.Name)
-              {
-                default:
-                  BoardMultiplier = 1.00;
-                break;
-                case "Bigger Board":
-                  BoardMultiplier = 1.75;
-                break;
-                case "Small Board":
-                  BoardMultiplier = 0.5;
-                break;
-                case "Tiny Board":
-                  BoardMultiplier = 0.25;
-                break;
-              }
-              RowNumber = ((int)(DefaultRows * BoardMultiplier));
-              ColumnNumber = ((int)(DefaultColumns * BoardMultiplier));
-              break;
-            }
-            else
-            {
-              RowNumber = DefaultRows;
-              ColumnNumber = DefaultColumns;
-            }
-          }
-          GameGrid.RowDefinitions.Clear();
-          GameGrid.ColumnDefinitions.Clear();
-
-          for (int i = 0; i < RowNumber; i++)
-          {
-            GameGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-          }
-
-          for (int j = 0; j < ColumnNumber; j++)
-          {
-            GameGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-          }
-
-          BoardArray = new int[RowNumber, ColumnNumber];
-        }
+        GameSettings currentSettings = JsonSerializer.Deserialize<GameSettings>(File.ReadAllText(settingsPath));
+        return currentSettings;
       }
-
-      private void DetermineSpeed()
+      else
       {
-        double speedMultiplier = 1;
-        double defaultSpeed = 3; // 3 columns/rows a second 
-        if(currentSettings.Modifiers != null)
-        {
-          foreach(ModifierItem Modifier in currentSettings.Modifiers)
-          {
-            if(Modifier.Name.Contains("Speed") || Modifier.Name.Contains("Slow"))
-            {
-              switch (Modifier.Name)
-              {
-                default:
-                  speedMultiplier = 1;
-                break;
-                case "Slowness":
-                  speedMultiplier = 0.5;
-                break;
-                case "Fast Speed":
-                  speedMultiplier = 2;
-                break;
-                case "Super Speed":
-                  speedMultiplier = 4;
-                break;
-              }
-              PlayerSpeed = speedMultiplier * defaultSpeed;
-            }
-          }
-        }
-      } 
-      private readonly GameSettings currentSettings = LoadSettings();
-
-      private void RunPreGameFunctions()
-      {
-        MakeBoard();
-        DetermineSpeed();
+        GameSettings defaultSettings = new GameSettings 
+        { 
+          CurrentColor = "#FF0000FF",
+          Modifiers = null,
+          Multiplier = 1,
+          Settings = null
+        };
+        File.WriteAllText(settingsPath, JsonSerializer.Serialize(defaultSettings));
+        return defaultSettings;
       }
     }
+
+    private readonly GameSettings currentSettings = LoadSettings();
+
+    private void RunPreGameFunctions()
+    {
+      MakeBoard();
+      DetermineSpeed();
+    }
+  }
 }
---
+++ ModifierLogic.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WindowsSnake
+{
+  public partial class GamePage
+  {
+    private void MakeBoard()
+    {
+      const int DefaultRows = 20;
+      const int DefaultColumns = 20;
+      double BoardMultiplier = 1.00;
+      int RowNumber = DefaultRows;
+      int ColumnNumber = DefaultColumns;
+      if (currentSettings.Modifiers != null)
+      {
+        foreach (ModifierItem Modifier in currentSettings.Modifiers)
+        {
+          if (Modifier.Name.Contains("Board"))
+          {
+            switch (Modifier.Name)
+            {
+              default:
+                BoardMultiplier = 1.00;
+                break;
+              case "Bigger Board":
+                BoardMultiplier = 1.75;
+                break;
+              case "Small Board":
+                BoardMultiplier = 0.5;
+                break;
+              case "Tiny Board":
+                BoardMultiplier = 0.25;
+                break;
+            }
+            RowNumber = ((int)(DefaultRows * BoardMultiplier));
+            ColumnNumber = ((int)(DefaultColumns * BoardMultiplier));
+            break;
+          }
+          else
+          {
+            RowNumber = DefaultRows;
+            ColumnNumber = DefaultColumns;
+          }
+        }
+        GameGrid.RowDefinitions.Clear();
+        GameGrid.ColumnDefinitions.Clear();
+
+        for (int i = 0; i < RowNumber; i++)
+        {
+          GameGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+        }
+
+        for (int j = 0; j < ColumnNumber; j++)
+        {
+          GameGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+        }
+
+        BoardArray = new int[RowNumber, ColumnNumber];
+      }
+    }
+
+    private void DetermineSpeed()
+    {
+      double speedMultiplier = 1;
+      double defaultSpeed = 3; // 3 columns/rows a second 
+      if (currentSettings.Modifiers != null)
+      {
+        foreach (ModifierItem Modifier in currentSettings.Modifiers)
+        {
+          if (Modifier.Name.Contains("Speed") || Modifier.Name.Contains("Slow"))
+          {
+            switch (Modifier.Name)
+            {
+              default:
+                speedMultiplier = 1;
+              break;
+              case "Slowness":
+                speedMultiplier = 0.5;
+              break;
+              case "Fast Speed":
+                speedMultiplier = 2;
+              break;
+              case "Super Speed":
+                speedMultiplier = 4;
+              break;
+            }
+            PlayerSpeed = speedMultiplier * defaultSpeed;
+          }
+          else
+          {
+            PlayerSpeed = defaultSpeed;
+          }
+        }
+      }
+    }
+  }
+}
